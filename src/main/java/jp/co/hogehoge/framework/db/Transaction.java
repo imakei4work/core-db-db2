ec955e6926d47c187f048b073407e51f621a38d1
Keisuke Imai <imakei4work@gmail.com>
2022-03-05 22:08:30 +0900

    戻り値を必要としないstartメソッドを追加

--- src/main/java/jp/co/hogehoge/framework/db/Transaction.java
+++ src/main/java/jp/co/hogehoge/framework/db/Transaction.java
@@ -18,8 +18,7 @@ public class Transaction {
 	protected static Logger logger = LogManager.getLogger(Transaction.class);
 
 	/** トランザクション・スタック */
-	private static final ThreadLocal<Stack<TransactionExecutor<?>>> TRANSACTION = ThreadLocal
-			.withInitial(() -> new Stack<>());
+	private static final ThreadLocal<Stack<Object>> TRANSACTION = ThreadLocal.withInitial(() -> new Stack<>());
 
 	/**
 	 * コンストラクタ
@@ -31,22 +30,22 @@ public class Transaction {
 	/**
 	 * トランザクションを実行する。
 	 * 
-	 * @param dao DAO
+	 * @param supplier トランザクション処理
 	 * @return SQL実行結果
 	 * @throws DatabaseConnectionException コネクションの取得に失敗した場合
 	 * @throws TransactionException        コネクション取得後、トランザクション内で何らかのエラーが発生した場合
 	 */
-	public static <R> R start(TransactionExecutor<R> transaction) {
-		Stack<TransactionExecutor<?>> stack = TRANSACTION.get();
+	public static <R> R start(TransactionSupplier<R> supplier) {
+		Stack<Object> stack = TRANSACTION.get();
 		DatabaseConnection conn = null;
 		R result = null;
 		try {
 			// コネクションを取得
 			conn = DatabaseConnection.getConnection();
 			// 生成したトランザクションをトランザクション・スタックへ追加
-			stack.push(transaction);
+			stack.push(supplier);
 			// トランザクションを実行
-			result = transaction.execute();
+			result = supplier.execute();
 			// トランザクション・スタックからポップ
 			stack.pop();
 			// トランザクションが先頭（トランザクション・スタックが空）の場合にコミット
@@ -82,12 +81,69 @@ public class Transaction {
 	}
 
 	/**
+	 * トランザクションを実行する。
+	 * 
+	 * @param consumer トランザクション処理
+	 * @throws DatabaseConnectionException コネクションの取得に失敗した場合
+	 * @throws TransactionException        コネクション取得後、トランザクション内で何らかのエラーが発生した場合
+	 */
+	public static void start(TransactionConsumer consumer) {
+		Stack<Object> stack = TRANSACTION.get();
+		DatabaseConnection conn = null;
+		try {
+			// コネクションを取得
+			conn = DatabaseConnection.getConnection();
+			// 生成したトランザクションをトランザクション・スタックへ追加
+			stack.push(consumer);
+			// トランザクションを実行
+			consumer.execute();
+			// トランザクション・スタックからポップ
+			stack.pop();
+			// トランザクションが先頭（トランザクション・スタックが空）の場合にコミット
+			if (stack.isEmpty()) {
+				conn.commit();
+			}
+		} catch (DatabaseConnectionException e) {
+			// コネクションの取得に失敗した場合、クローズやロールバック等、コネクションに対する処理が実行不可であるため、
+			// トランザクション・スタックのクリアのみ実行して例外をスローする
+			stack.clear();
+			throw new TransactionException(e, Message.DBE00001);
+		} catch (TransactionException e) {
+			// ネストしたトランザクションにてエラーが発生した場合はそのままスロー
+			throw e;
+		} catch (Exception e) {
+			// トランザクション内でエラーが発生した場合トランザクション・スタックをクリアしてロールバックを行う
+			stack.clear();
+			try {
+				conn.rollback();
+				throw new TransactionException(e, Message.DBE00007);
+			} catch (SQLException se) {
+				se.addSuppressed(e);
+				throw new TransactionException(se, Message.DBE00008);
+			}
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// NOP
+			}
+		}
+	}
+
+	/**
 	 * トランザクション実行インタフェース。
 	 *
 	 * @param <R> SQL実行結果のデータ型
 	 */
-	public static interface TransactionExecutor<R> {
+	public static interface TransactionSupplier<R> {
 		public R execute() throws SQLException;
+	}
+
+	/**
+	 * トランザクション実行インタフェース。
+	 */
+	public static interface TransactionConsumer {
+		public void execute() throws SQLException;
 	}
 
 }
