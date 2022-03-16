34468ed036e0e4f537675c3a1f8a67d8ba2159fa
Keisuke Imai <imakei4work@gmail.com>
2022-03-17 08:24:02 +0900

    TestCommandのテストケースを追加

--- src/test/java/jp/co/hogehoge/framework/db/TestCommand.java
+++ src/test/java/jp/co/hogehoge/framework/db/TestCommand.java
@@ -2,11 +2,14 @@ package jp.co.hogehoge.framework.db;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import java.math.BigDecimal;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -15,6 +18,7 @@ import org.junit.Test;
 
 import jp.co.hogehoge.framework.test.db.TestDB;
 import untest.clazz.AllTypeEntity;
+import untest.clazz.NotSupportTypeEntity;
 import untest.clazz.UnitTestSql;
 import untest.conf.DBConfig;
 
@@ -78,17 +82,216 @@ public class TestCommand {
 	 */
 	@Test
 	public void select_01() throws SQLException {
-		// arrange
-		Command<Optional<AllTypeEntity>> actual = Command.select();
 		// act
-		DatabaseConnection conn = DatabaseConnection.getConnection();
-		Optional<AllTypeEntity> result = actual.execute(conn,
-				UnitTestSql.SELECT_001.getSql(),
-				initData1.toMap(),
-				ResultSetParser.toEntity(AllTypeEntity.class));
-		conn.commit().close();
+		Command<Optional<AllTypeEntity>> actual = Command.select();
 		// assert
-		assertThat("検索結果が想定通りであること", result.get(), equalTo(initData1));
+		try (DatabaseConnection conn = DatabaseConnection.getConnection()) {
+			Optional<AllTypeEntity> result = actual.execute(conn,
+					UnitTestSql.SELECT_001.getSql(),
+					initData1.toMap(),
+					ResultSetParser.toEntity(AllTypeEntity.class));
+			conn.commit();
+			assertThat("検索結果が想定通りであること", result.get(), equalTo(initData1));
+		}
+	}
+
+	/**
+	 * 更新結果が想定通りであること。
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void update_01() throws SQLException {
+		// arrange
+		AllTypeEntity data = initData1.clone().setCharColumn("00000");
+		// act
+		Command<Integer> actual = Command.update();
+		// assert
+		try (DatabaseConnection conn = DatabaseConnection.getConnection()) {
+			Integer result = actual.execute(conn, UnitTestSql.UPDATE_001.getSql(), data.toMap(), null);
+			conn.commit();
+			assertThat("更新結果が想定通りであること", result, equalTo(1));
+		}
+	}
+
+	/**
+	 * 登録結果が想定通りであること。
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void insert_01() throws SQLException {
+		// arrange
+		AllTypeEntity data = initData1.clone().setVarcharColumn("00000");
+		// act
+		Command<Integer> actual = Command.insert();
+		// assert
+		try (DatabaseConnection conn = DatabaseConnection.getConnection()) {
+			Integer result = actual.execute(conn, UnitTestSql.INSERT_001.getSql(), data.toMap(), null);
+			conn.commit();
+			assertThat("登録結果が想定通りであること", result, equalTo(1));
+		}
+	}
+
+	/**
+	 * 削除結果が想定通りであること。
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void delete_01() throws SQLException {
+		// act
+		Command<Integer> actual = Command.delete();
+		// assert
+		try (DatabaseConnection conn = DatabaseConnection.getConnection()) {
+			Integer result = actual.execute(conn, UnitTestSql.DELETE_002.getSql(), initData1.toMap(), null);
+			conn.commit();
+			assertThat("削除結果が想定通りであること", result, equalTo(1));
+		}
+	}
+
+	/**
+	 * 作成結果が想定通りであること。
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void create_01() throws SQLException {
+		// arrange
+		Transaction.execute(() -> UnitTestSql.DLOP_001.execute());
+		// act
+		Command<Integer> actual = Command.create();
+		// assert
+		try (DatabaseConnection conn = DatabaseConnection.getConnection()) {
+			Integer result = actual.execute(conn, UnitTestSql.CREATE_001.getSql(), null, null);
+			conn.commit();
+			assertThat("作成結果が想定通りであること", result, equalTo(0));
+		}
+	}
+
+	/**
+	 * 消去結果が想定通りであること。
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void drop_01() throws SQLException {
+		// act
+		Command<Integer> actual = Command.drop();
+		// assert
+		try (DatabaseConnection conn = DatabaseConnection.getConnection()) {
+			Integer result = actual.execute(conn, UnitTestSql.DLOP_001.getSql(), null, null);
+			conn.commit();
+			assertThat("消去結果が想定通りであること", result, equalTo(0));
+		}
+	}
+
+	/**
+	 * パラメータがnullの場合に正常に処理が終了すること。
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void setPreparedStatement_01() throws SQLException {
+		// arrange
+		Command<Integer> command = Command.create();
+		try (DatabaseConnection conn = DatabaseConnection.getConnection()) {
+			try (PreparedStatement pstmt = conn.prepareStatement(UnitTestSql.CREATE_001.getSql())) {
+				// act
+				command.setPreparedStatement(pstmt, null);
+				// assert
+				conn.commit();
+			}
+		}
+	}
+
+	/**
+	 * パラメータがEmptyの場合でも正常に処理が終了すること。
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void setPreparedStatement_02() throws SQLException {
+		// arrange
+		Command<Integer> command = Command.create();
+		try (DatabaseConnection conn = DatabaseConnection.getConnection()) {
+			try (PreparedStatement pstmt = conn.prepareStatement(UnitTestSql.CREATE_001.getSql())) {
+				// act
+				command.setPreparedStatement(pstmt, new HashMap<>());
+				// assert
+				conn.commit();
+			}
+		}
+	}
+
+	/**
+	 * 全てのデータ型に対して正常に設定が出来ること。
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void setPreparedStatement_03() throws SQLException {
+		// arrange
+		Command<Integer> command = Command.create();
+		try (DatabaseConnection conn = DatabaseConnection.getConnection()) {
+			try (PreparedStatement pstmt = conn.prepareStatement(UnitTestSql.INSERT_001.getSql())) {
+				// act
+				command.setPreparedStatement(pstmt, initData1.toMap());
+				// assert
+				conn.commit();
+			}
+		}
+	}
+
+	/**
+	 * 全てのデータ型がnull（主キーのみ非null）の場合に正常に設定が出来ること。
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void setPreparedStatement_04() throws SQLException {
+		// arrange
+		AllTypeEntity data = (new AllTypeEntity()).setVarcharColumn("55555")
+				.setCharColumn(null)
+				.setIntegerColumn(null)
+				.setDateColumn(null)
+				.setDoubleColumn(null)
+				.setClobColumn(null)
+				.setBlobColumn(null)
+				.setBigDecimalColumn(null)
+				.setLongColumn(null)
+				.setTimestampColumn(null);
+
+		Command<Integer> command = Command.create();
+		try (DatabaseConnection conn = DatabaseConnection.getConnection()) {
+			try (PreparedStatement pstmt = conn.prepareStatement(UnitTestSql.INSERT_001.getSql())) {
+				// act
+				command.setPreparedStatement(pstmt, data.toMap());
+				// assert
+				conn.commit();
+			}
+		}
+	}
+
+	/**
+	 * サポートしていないデータ型がEntityに含まれていた場合はSQLExceptionがスローされること。
+	 * 
+	 * @throws SQLException
+	 */
+	@Test(expected = SQLException.class)
+	public void setPreparedStatement_05() throws SQLException {
+		NotSupportTypeEntity entity = (new NotSupportTypeEntity()).setVarcharColumn(initData1);
+		Command<Integer> command = Command.create();
+		try (DatabaseConnection conn = DatabaseConnection.getConnection()) {
+			try (PreparedStatement pstmt = conn.prepareStatement(UnitTestSql.SELECT_001.getSql())) {
+				try { // act
+					command.setPreparedStatement(pstmt, entity.toMap());
+					fail("実行された場合はNG");
+				} finally {
+					conn.commit();
+				}
+			}
+		}
 	}
 
 }
---
+++ src/test/java/untest/clazz/NotSupportTypeEntity.java
@@ -0,0 +1,18 @@
+package untest.clazz;
+
+import jp.co.hogehoge.framework.db.Entity;
+
+public class NotSupportTypeEntity extends Entity {
+
+	private AllTypeEntity varcharColumn = null;
+
+	public AllTypeEntity getVarcharColumn() {
+		return varcharColumn;
+	}
+
+	public NotSupportTypeEntity setVarcharColumn(AllTypeEntity varcharColumn) {
+		this.varcharColumn = varcharColumn;
+		return this;
+	}
+
+}
